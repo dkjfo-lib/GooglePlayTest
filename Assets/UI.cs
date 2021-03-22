f2185bf7c391bb6ff2a9d254eaf7dc6665745ed4
Mike <mihailfd@gmail.com>
2021-03-22 18:39:08 +0300

    initial project

---
+++ Assets/UI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class UI : MonoBehaviour
+{
+    Animator UIAnimator;
+
+    public bool inMenu = false;
+
+    private void Start()
+    {
+        UIAnimator = GetComponent<Animator>();
+        ShowMenu(inMenu);
+    }
+
+    public void ShowMenu(bool newState)
+    {
+        if (newState == UIAnimator.GetBool("Menu Open")) return;
+
+        inMenu = newState;
+        UIAnimator.SetBool("Menu Open", newState);
+        UIAnimator.SetTrigger("Toggle Menu");
+    }
+
+    public void ExitApp()
+    {
+        Debug.LogWarning("Exiting Game!");
+        Application.Quit();
+    }
+}
